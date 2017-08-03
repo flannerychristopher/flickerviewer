7b1e013a42f3a3976dc552ca1a61813c176dd3d2
chris flan <flanthedev@gmail.com>
2017-08-03 14:19:37 +0800

    add search function

--- js/script.js
+++ js/script.js
@@ -22,5 +22,28 @@ $(document).ready(function() {
             );
   });
 
+  $('form').submit( function(evt) {
+    evt.preventDefault();
+    let searchInput = $('#search').val();
+
+    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+              {
+                tags: searchInput,
+                format: "json",
+              },
+              function(data) {
+                let photoHTML = '<ul>';
+                $.each(data.items, function(i, photo)  {
+                  photoHTML += '<li>';
+                  photoHTML += '<a href="' + photo.link + '">';
+                  photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+                });
+                photoHTML += '</ul>';
+                $('#photos').html(photoHTML);
+              }
+
+            );
+  });
+
 
 }); // close document ready
